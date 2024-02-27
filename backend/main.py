6387695d945680868e469cee590f3a123d684625
task <taskevichoff@gmail.com>
2024-02-27 06:45:17 +0300

    Модуль авторизации и минорные исправления

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
-from routes import main_routes
+from routes import main_routes, admin_routes, credentials_routes
 from backend.backend.models import *
 from backend.backend.logger import *
 
 app = FastAPI()
 
 app.include_router(main_routes.router)
+app.include_router(admin_routes.router)
+app.include_router(credentials_routes.router)
 
 
 @app.on_event("startup")
--- backend/routes/credentials_routes.py
+++ backend/routes/credentials_routes.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from ..backend.schemas import *
+from backend.backend.schemas import *
 
 router = APIRouter()
 
