e25eb79eb00f0c6f75e97292c3433ad54b363b3e
Kayoshi03 <r0n1ronov1@gmail.com>
2024-03-04 17:48:57 +0300

    router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,25 @@
 import MainPage from "./pages/MainPage/MainPage";
+import {Route, Routes} from "react-router";
+import SignInPage from "./pages/SignInPage/SignInPage";
+import React from "react";
+import MainLayout from "./pages/MainLayout/MainLayout";
+import SignUpPage from "./pages/SignUpPage/SignUpPage";
 
 const App = () => {
 
     return (
-            <>
-                <MainPage></MainPage>
-            </>
+            <Routes>
+                 {/*mainRoute*/}
+                <Route path="/" element={<MainLayout/>}>
+                    <Route index path="/main" element={<MainPage/>}/>
+                </Route>
+                {/*Auth*/}
+                <Route path="/panel/">
+                    <Route path="signin" element={<SignInPage/>}/>
+                    <Route path="signup" element={<SignUpPage/>}/>
+                    <Route path="recovery"/>
+                </Route>
+            </Routes>
         )
 };
 
--- frontend/src/pages/MainPage/MainPage.jsx
+++ frontend/src/pages/MainPage/MainPage.jsx
@@ -4,26 +4,7 @@ import {Link, NavLink} from "react-router-dom";
 const MainPage = () => {
     return (
         <>
-            <header className="flex">
-                <div className="flex container">
-                    <div className="logoName">
-                        <h2>{string.NameProject}</h2>
-                    </div>
-                    <li className="navList flex">
-                        {string.NavMenu.map((nav, index) => (
-                            <NavLink className="navItem " key={index} to={nav.path}>{nav.title}</NavLink>
-                        ))}
-                    </li>
-                    <div>
-                        <button><Link to="auth">Авторизоваться</Link></button>
-                        <button><Link to="reg">Зарегестрироваться</Link></button>
-                    </div>
-                </div>
-            </header>
-            <main></main>
-            <footer>
-                <p>Footer</p>
-            </footer>
+
         </>
     )
 };
