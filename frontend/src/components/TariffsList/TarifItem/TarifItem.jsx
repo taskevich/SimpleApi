a467790f41fdeb316d58b029e058e62c2df54da5
Kayoshi03 <r0n1ronov1@gmail.com>
2024-03-10 20:27:37 +0300

    h2 в тарифах центровал

--- frontend/src/components/TariffsList/TarifItem/TarifItem.jsx
+++ frontend/src/components/TariffsList/TarifItem/TarifItem.jsx
@@ -4,6 +4,11 @@ const TarifItem = ({item}) => {
     return(
         <div className="tarrifItem flex">
             <h3>{item.title}</h3>
+            <ul>
+                {item.list.map((punkt, index) => (
+                    <li key={index}>{punkt}</li>
+                ))}
+            </ul>
             <p>{item.price}Руб/мес</p>
             <button className="btn">Выбрать</button>
         </div>
